01c392f636e3ddf2d5fa6b73cb2e9bc3aa0c1f7b
yeasin50 <yeasinsheikh50@gmail.com>
2023-08-18 21:04:06 +0600

    reorg

--- lib/chat_bot_app.dart
+++ lib/chat_bot_app.dart
@@ -1,10 +1,6 @@
-import 'package:f3/locator.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'features/chat/presentation/pages/conversation_page.dart';
-import 'firebase_options.dart';
+import 'features/chat/presentation/pages/chat_page.dart';
 
 void main(List<String> args) {
   runApp(const ChatBotApp());
@@ -14,37 +10,14 @@ class ChatBotApp extends MaterialApp {
   const ChatBotApp({super.key});
 
   @override
-  Widget? get home => _InitPage();
+  Widget? get home => const ChatPage();
+
+  @override
+  bool get debugShowCheckedModeBanner => false;
 
   @override
   ThemeData? get theme => ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
       );
-}
-
-class _InitPage extends StatelessWidget {
-  const _InitPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    Future<void> _init() async {
-      await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-      await FirebaseAuth.instance.signInAnonymously();
-      setup();
-    }
-
-    return FutureBuilder(
-      future: _init(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return const ConversationPage();
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
-    );
-  }
 }
